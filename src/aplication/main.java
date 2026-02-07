c085d3b1197f6bb8a6627e6b0f9ef05e9beefefe
kriztinajg-jpg <kriztinajg@gmail>
2026-02-07 14:09:32 -0500

    Repaso java

--- src/aplication/main.java
+++ src/aplication/main.java
@@ -2,7 +2,22 @@ package aplication;
 
 public class main {
         public static void main(String[] args) {
-            System.out.println("Hello world");
+            System.out.println("Hello");
+            String nombre= "Cristina";
+            int edad = 30;
+            boolean femenino= true;
+            System.out.println("Hola mi nombre es " +nombre + " mi edad es "+ edad );
+            int fechaActual= 2025;
+            int fechaNacimiento = 1995;
+            int edadActual= fechaActual-fechaNacimiento;
+            System.out.println( "mi edad actual es " + edadActual);
+            double PI= 3.14;
+            for (int i = 1; i <= 10; i++)
+                System.out.println("Número: " + i);
+
+
+
+
         }
     }
 
